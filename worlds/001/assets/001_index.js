ae6fe1341c6f27da1d6395b56e006c7203d94e02
arisaito <saharu54@live.jp>
2019-05-05 18:15:29 +0900

    001 update

--- worlds/001/assets/001_index.js
+++ worlds/001/assets/001_index.js
@@ -6,10 +6,11 @@ $(function() {
 });
 
 window.onload = function() {
+  let objColor = ["#EA59F7", "#E046AB", "#FC4E69", "#A946E0", "#8E4EFC"];
+  let randomColor = objColor[Math.floor(Math.random() * objColor.length)];
   var fragment = document.createDocumentFragment();
   var scene = document.querySelector("a-scene");
   var wrapper = document.querySelector("#sphereWrapper");
-
   for (var i = 0; i < 100; i++) {
     var sphere = document.createElement("a-sphere");
     sphere.setAttribute("position", {
@@ -21,6 +22,8 @@ window.onload = function() {
     sphere.setAttribute("material", {
       metalness: 1
     });
+    sphere.setAttribute("color", randomColor);
+    fragment.appendChild(sphere);
   }
   wrapper.appendChild(fragment);
 };
