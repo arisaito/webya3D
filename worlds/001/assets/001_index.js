b20d0e01a938a30ce5521b3248138232bb73b694
arisaito <saharu54@live.jp>
2019-05-06 22:26:40 +0900

    001 update

--- worlds/001/assets/001_index.js
+++ worlds/001/assets/001_index.js
@@ -7,22 +7,24 @@ $(function() {
 
 window.onload = function() {
   let objColor = ["#EA59F7", "#E046AB", "#FC4E69", "#A946E0", "#8E4EFC"];
-  let randomColor = objColor[Math.floor(Math.random() * objColor.length)];
   var fragment = document.createDocumentFragment();
   var scene = document.querySelector("a-scene");
   var wrapper = document.querySelector("#sphereWrapper");
-  for (var i = 0; i < 100; i++) {
+  for (var i = 0; i < 180; i++) {
     var sphere = document.createElement("a-sphere");
     sphere.setAttribute("position", {
-      x: Math.random() * 20 - 10,
+      x: Math.random() * 16 * 2 - 16,
       y: Math.random() * 4,
-      z: Math.random() * 20 - 10
+      z: Math.random() * 16 * 2 - 16
     });
     sphere.setAttribute("scale", { x: 0.5, y: 0.5, z: 0.5 });
     sphere.setAttribute("material", {
-      metalness: 1
+      metalness: 0.93
     });
-    sphere.setAttribute("color", randomColor);
+    sphere.setAttribute(
+      "color",
+      objColor[Math.floor(Math.random() * objColor.length)]
+    );
     fragment.appendChild(sphere);
   }
   wrapper.appendChild(fragment);
