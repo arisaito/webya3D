aebeb3bc8daea3cae556c8b6b7351d0fe32b867d
arisaito <saharu54@live.jp>
2019-05-09 16:47:12 +0900

    002 update

--- worlds/002/assets/002_index.js
+++ worlds/002/assets/002_index.js
@@ -4,21 +4,3 @@ $(function() {
     $("#modal").fadeOut();
   });
 });
-
-window.onload = function() {
-  var fragment = document.createDocumentFragment();
-  var scene = document.querySelector("a-scene");
-  var wrapper = document.querySelector("#sphereWrapper");
-  for (var i = 0; i < 800; i++) {
-    var sphere = document.createElement("a-sphere");
-    sphere.setAttribute("position", {
-      x: Math.random() * 1 * 2 - 1,
-      y: Math.random() * 1 * 2 - 1,
-      z: Math.random() * 1 * 2 - 1
-    });
-    sphere.setAttribute("scale", { x: 0.03, y: 0.03, z: 0.03 });
-    sphere.setAttribute("color", "#A3B4BF");
-    fragment.appendChild(sphere);
-  }
-  wrapper.appendChild(fragment);
-};
